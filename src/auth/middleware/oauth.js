53fd6c41994a9b95c80722ca047bed3ce3655af3
Davee Sok <daveesok@gmail.com>
2020-09-23 18:37:18 -0700

    updated

--- src/auth/middleware/oauth.js
+++ src/auth/middleware/oauth.js
@@ -11,19 +11,16 @@ const REMOTE_API = process.env.REMOTE_API;
 
 
 module.exports = async (req, res, next) => {
-  // Get the code
   let code = req.query.code;
   console.log('(1) CODE:', code);
 
-  // Exchange the code for a token
-  let remoteToken = await exchangeCodeForToken(code);
-  console.log('(2)', remoteToken);
+  let gitHubToken = await exchangeCodeForToken(code);
+  console.log('(2)', gitHubToken);
 
-  // Get user info from GitHub
-  let remoteUser = await getRemoteUser(remoteToken);
-  console.log('(3)', remoteUser);
+  let remoteUser = await getRemoteUser(gitHubToken);
+  console.log('(3) remoteUser: ', remoteUser);
+
   // Connect that with our database
-
   let localUser = await getLocalUser(remoteUser.login);
   console.log('(4)', localUser);
 
@@ -32,7 +29,6 @@ module.exports = async (req, res, next) => {
 
 
 async function exchangeCodeForToken(code) {
-
   let tokenResponse = await superagent.post(TOKEN_SERVER)
     .send({
       code: code,
@@ -40,23 +36,18 @@ async function exchangeCodeForToken(code) {
       client_secret: CLIENT_SECRET,
       redirect_uri: REDIRECT_URI,
       state: STATE,
-      grant_type: 'authorization_code',
+      // grant_type: 'authorization_code',
     });
-
   let access_token = tokenResponse.body.access_token;
-
   return access_token;
-
 }
 
 async function getRemoteUser(token) {
-
   let userResponse = await superagent.get(REMOTE_API)
     .set('user-agent', 'express-server')
     .set('Authorization', `token ${token}`);
 
   let user = userResponse.body;
-
   return user;
 }
 
